ddeef45918395a6a4e736ca8a2a66c60f021940a
Bryce Klinker <klinker.bryce@principal.com>
2020-07-17 11:54:51 -0500

    SPA can now call the api from the browser. Some issues still happen when handling the response but cors is not an issue now

--- aws/spa-with-cognito-auth/infrastructure/common/api_gateway.tf
+++ aws/spa-with-cognito-auth/infrastructure/common/api_gateway.tf
@@ -1,135 +1,60 @@
-resource "aws_api_gateway_rest_api" "lambda_api" {
+resource "aws_api_gateway_rest_api" "gateway" {
   name = "${local.namespace}-lambda-rest"
   description = "Sample API for securing lambdas"
 }
 
-resource "aws_api_gateway_resource" "lambda_resource_proxy" {
-  parent_id = aws_api_gateway_rest_api.lambda_api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  parent_id = aws_api_gateway_rest_api.gateway.root_resource_id
   path_part = "{proxy+}"
 }
 
-//resource "aws_api_gateway_authorizer" "lambda_authorizer" {
-//  name = "${local.namespace}-authorizer"
-//  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-//  type = "COGNITO_USER_POOLS"
-//
-//  provider_arns = [
-//    aws_cognito_user_pool.spa.arn
-//  ]
-//}
-
-//resource "aws_api_gateway_method" "options_method" {
-//  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-//  resource_id = aws_api_gateway_resource.lambda_resource.id
-//  http_method = "OPTIONS"
-//  authorization = "NONE"
-//}
-//
-//resource "aws_api_gateway_method_response" "options_method_response" {
-//  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-//  resource_id = aws_api_gateway_resource.lambda_resource.id
-//  http_method = aws_api_gateway_method.options_method.http_method
-//  status_code = "200"
-//
-//  response_models = {
-//    "application/json" = "Empty"
-//  }
-//
-//  response_parameters = {
-//    "method.response.header.Access-Control-Allow-Headers" = true,
-//    "method.response.header.Access-Control-Allow-Methods" = true,
-//    "method.response.header.Access-Control-Allow-Origin" = true
-//  }
-//}
-//
-//resource "aws_api_gateway_integration" "options_integration" {
-//  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-//  resource_id = aws_api_gateway_resource.lambda_resource.id
-//  http_method = aws_api_gateway_method.options_method.http_method
-//  type = "MOCK"
-//
-//  depends_on = [
-//    aws_api_gateway_method.options_method
-//  ]
-//}
-//
-//resource "aws_api_gateway_integration_response" "options_integration_resposne" {
-//  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-//  resource_id = aws_api_gateway_resource.lambda_resource.id
-//  http_method = aws_api_gateway_method.options_method.http_method
-//  status_code = aws_api_gateway_method_response.options_method_response.status_code
-//
-//  response_parameters = {
-//    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-//    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE,PATCH'",
-//    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-//  }
-//
-//  depends_on = [
-//    aws_api_gateway_method_response.options_method_response
-//  ]
-//}
-
-resource "aws_api_gateway_method" "lambda_proxy_method" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-  resource_id = aws_api_gateway_resource.lambda_resource_proxy.id
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
   http_method = "ANY"
   authorization = "NONE"
-
-//  authorization = "COGNITO_USER_POOLS"
-//  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
-//
-//  authorization_scopes = [
-//    join(" ", aws_cognito_resource_server.lambda_api.scope_identifiers)
-//  ]
 }
 
-resource "aws_api_gateway_method_response" "lambda_method_proxy_response" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-  resource_id = aws_api_gateway_resource.lambda_resource_proxy.id
-  http_method = aws_api_gateway_method.lambda_proxy_method.http_method
-  status_code = "200"
-
-  depends_on = [
-    aws_lambda_function.api_lambda
-  ]
-}
-
-resource "aws_api_gateway_integration" "lambda_proxy_integration" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-  resource_id = aws_api_gateway_resource.lambda_resource_proxy.id
-  http_method = aws_api_gateway_method.lambda_proxy_method.http_method
-  uri = aws_lambda_function.api_lambda.invoke_arn
-  type = "AWS_PROXY"
-
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
   // The integration method for lambdas is always POST
   // The HTTP Method used to trigger the api gateway will be used for identifying which express route to hit.
   integration_http_method = "POST"
-
-  depends_on = [
-    aws_api_gateway_method.lambda_proxy_method
-  ]
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.api_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_integration_response" "lambda_proxy_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-  resource_id = aws_api_gateway_resource.lambda_resource_proxy.id
-  http_method = aws_api_gateway_method.lambda_proxy_method.http_method
-  status_code = aws_api_gateway_method_response.lambda_method_proxy_response.status_code
+resource "aws_api_gateway_method" "root" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  resource_id = aws_api_gateway_rest_api.gateway.root_resource_id
+  http_method = "ANY"
+  authorization = "NONE"
+}
 
-
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  resource_id = aws_api_gateway_method.root.resource_id
+  http_method = aws_api_gateway_method.root.http_method
+  // The integration method for lambdas is always POST
+  // The HTTP Method used to trigger the api gateway will be used for identifying which express route to hit.
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.api_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
   stage_name = var.env
 
   // Using the timestamp here to make sure that the api is deployed each time terraform is applied
   stage_description = "Stage deployed at ${timestamp()}"
 
   depends_on = [
-    aws_api_gateway_integration.lambda_proxy_integration
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root
   ]
 }
 
@@ -138,7 +63,7 @@ resource "aws_lambda_permission" "lambda_api_gateway_permission" {
   function_name = aws_lambda_function.api_lambda.function_name
   principal = "apigateway.amazonaws.com"
   statement_id = "AllowLambdaExecutionFromApiGateway"
-  source_arn = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/*"
 
   depends_on = [
     aws_lambda_function.api_lambda
--- aws/spa-with-cognito-auth/lambda/lambda.js
+++ aws/spa-with-cognito-auth/lambda/lambda.js
@@ -1,26 +1,6 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const { app } = require('./src/app');
+const { getMimeTypesToGzip } = require('./src/mime-types')
 
-const binaryMimeTypes = [
-    'application/javascript',
-    'application/json',
-    'application/octet-stream',
-    'application/xml',
-    'font/eot',
-    'font/opentype',
-    'font/otf',
-    'image/jpeg',
-    'image/png',
-    'image/svg+xml',
-    'text/comma-separated-values',
-    'text/css',
-    'text/html',
-    'text/javascript',
-    'text/plain',
-    'text/text',
-    'text/xml'
-]
-
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
-
+const server = awsServerlessExpress.createServer(app, null, getMimeTypesToGzip());
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
